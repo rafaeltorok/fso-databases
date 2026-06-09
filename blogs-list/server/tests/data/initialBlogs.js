9467c55bdfab95250c902f89c6e3cd00d85cf784
rafaeltorok <rafaeltorok@gmail.com>
2026-06-09 11:46:08 -0300

    refactor: removed the current test helpers and create a new script to handle the test data - move all future helper scripts to a proper folder

---
+++ blogs-list/server/tests/data/initialBlogs.js
@@ -0,0 +1,22 @@
+const initialBlogs = [
+  {
+    title: "Test blog",
+    author: "The Tester",
+    url: "https://testingblogs.com",
+    likes: 10,
+  },
+  {
+    title: "Another blog",
+    author: "The Blogger",
+    url: "https://anotherblog.com",
+    likes: 25,
+  },
+  {
+    title: "My blog",
+    author: "Myself",
+    url: "https://myblog.com",
+    likes: 1,
+  },
+];
+
+export default initialBlogs;
---
+++ blogs-list/server/tests/data/initialUsers.js
@@ -0,0 +1,14 @@
+const initialUsers = [
+  {
+    username: "admin",
+    name: "Administrator",
+    password: "admin",
+  },
+  {
+    username: "newuser",
+    name: "New User",
+    password: "newuser",
+  }
+];
+
+export default initialUsers;
