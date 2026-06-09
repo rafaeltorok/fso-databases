541567acc71602cca19720f189160e6c974ad54b
rafaeltorok <rafaeltorok@gmail.com>
2026-06-09 11:46:48 -0300

    update: improve the tests reset route to restart the id values

--- blogs-list/server/src/controllers/tests.js
+++ blogs-list/server/src/controllers/tests.js
@@ -2,16 +2,16 @@
 import express from "express";
 
 // Models
-import Blog from "../models/blog";
-import User from "../models/user";
+import Blog from "../models/blog.js";
+import User from "../models/user.js";
 
 const testsRouter = express.Router();
 
 // Routes
 testsRouter.post("/reset", async (req, res, next) => {
   try {
-    await Blog.destroy();
-    await User.destroy();
+    await Blog.truncate({ restartIdentity: true });
+    await User.truncate({ restartIdentity: true });
 
     res.status(204).end();
   } catch (err) {
