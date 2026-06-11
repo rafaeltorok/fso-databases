0c7d4e834a08d8e8b27f073017e5f8a4d06d8ec3
Rafael Torok <rafaeltorok@gmail.com>
2026-06-11 11:51:56 -0300

    Add Docker Composer dev orchestration - include dev PSQL database with persistent data storage - include Nginx as a proxy server to the backend - update: the db script to accept non-ssl connections, to be compatible with the PSQL Docker container

--- notes-app/server/util/db.js
+++ notes-app/server/util/db.js
@@ -1,14 +1,30 @@
 import Sequelize from "sequelize";
 import { DATABASE_URL } from "./config.js";
 
-export const sequelize = new Sequelize(DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+export let sequelize;
+
+if (process.env.DATABASE_SSL === "true") {
+  // SSL is required for remote databases
+  sequelize = new Sequelize(DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-  },
-});
+  });
+} else if (process.env.DATABASE_SSL === "false") {
+  // Disable SSL connections for the Docker orchestrations
+  sequelize = new Sequelize(DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: false,
+    },
+  });
+} else {
+  throw new Error({ error: "The DATABASE_SSL env variable must be set to either true or false" });
+}
 
 export const connectToDatabase = async () => {
   try {
