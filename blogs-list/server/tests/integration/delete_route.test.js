d2af492833db0ab56ec756311eb3797935aa0496
rafaeltorok <rafaeltorok@gmail.com>
2026-06-11 17:52:55 -0300

    fix: the delete route test suite by adding auth tokens

--- blogs-list/server/tests/integration/delete_route.test.js
+++ blogs-list/server/tests/integration/delete_route.test.js
@@ -17,6 +17,9 @@ import initialUsers from "../data/initialUsers.js";
 
 const api = supertest(app);
 
+// Global variables
+let loggedUser;
+
 // Helper functions
 async function getAmount(route) {
   const getResponse = await api.get(`/api/${route}`);
@@ -27,6 +30,24 @@ async function getAmount(route) {
 before(async () => {
   await setupDb();
   await api.post("/api/tests/reset");
+
+  // Create a new user for the tests
+  const user = initialUsers[0];
+
+  await api
+    .post("/api/users")
+    .send(user)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  // Log in the user and store the auth token
+  const loginResponse = await api
+    .post("/api/login")
+    .send({ username: user.username, password: user.password })
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+
+  loggedUser = loginResponse.body;
 });
 
 // Close the database connection after all tests have been finished
@@ -38,7 +59,7 @@ after(async () => {
 describe("the Blogs DELETE route", () => {
   beforeEach(async () => {
     // Remove all blogs before each test
-    await Blog.truncate({ restartIdentity: true });
+    await Blog.truncate({ restartIdentity: true, cascade: true });
   });
 
   test("a blog can be removed", async () => {
@@ -51,6 +72,7 @@ describe("the Blogs DELETE route", () => {
     const postResponse = await api
       .post("/api/blogs")
       .send(blog)
+      .set("Authorization", `Bearer ${loggedUser.token}`)
       .expect(201)
       .expect("Content-Type", /application\/json/);
 
@@ -61,6 +83,7 @@ describe("the Blogs DELETE route", () => {
     // Remove the blog
     await api
       .delete(`/api/blogs/${Number(postResponse.body.id)}`)
+      .set("Authorization", `Bearer ${loggedUser.token}`)
       .expect(204);
 
     // Assert the blogs length has decreased
@@ -71,12 +94,14 @@ describe("the Blogs DELETE route", () => {
   test("a non-existing id should return a proper status code", async () => {
     await api
       .delete("/api/blogs/0")
+      .set("Authorization", `Bearer ${loggedUser.token}`)
       .expect(404);
   });
 
   test("an invalid ID format should return a proper error message", async () => {
     const response = await api
       .delete("/api/blogs/my_blog")
+      .set("Authorization", `Bearer ${loggedUser.token}`)
       .expect(400)
       .expect("Content-Type", /application\/json/);
 
@@ -87,18 +112,51 @@ describe("the Blogs DELETE route", () => {
   test("a negative ID should return a proper error message", async () => {
     const response = await api
       .delete("/api/blogs/-1")
+      .set("Authorization", `Bearer ${loggedUser.token}`)
       .expect(400)
       .expect("Content-Type", /application\/json/);
 
     // Assert an error message is within the response
     assert.strictEqual(response.body.error, "Invalid ID format");
   });
+
+  test("a non-authorized user should not be able to delete a blog", async () => {
+    const blog = initialBlogs[0];
+
+    // Store the initial blogs length
+    const initialAmount = await getAmount("blogs");
+
+    // Add a blog to be removed
+    const postResponse = await api
+      .post("/api/blogs")
+      .send(blog)
+      .set("Authorization", `Bearer ${loggedUser.token}`)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    // Confirm the blogs length has increased
+    let currentAmount = await getAmount("blogs");
+    assert.strictEqual((initialAmount + 1), currentAmount);
+
+    // Try to delete a blog
+    const response = await api
+      .delete("/api/blogs/1")
+      .expect(401)
+      .expect("Content-Type", /application\/json/);
+
+    // Assert an error message is within the response
+    assert.strictEqual(response.body.error, "token missing");
+
+    // Confirm the blogs length has not changed
+    currentAmount = await getAmount("blogs");
+    assert.strictEqual((initialAmount + 1), currentAmount);
+  });
 });
 
 describe("the Users DELETE route", () => {
   beforeEach(async () => {
     // Remove all users before each test
-    await User.truncate({ restartIdentity: true });
+    await User.truncate({ restartIdentity: true, cascade: true });
   });
 
   test("an user can be removed", async () => {
