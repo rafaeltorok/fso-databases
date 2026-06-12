88a10a518d0e90eb088a1cdee473ebb1806cbaed
Rafael Torok <rafaeltorok@gmail.com>
2026-06-12 08:46:16 -0300

    update: add new tests for the users put route

--- blogs-list/server/tests/integration/put_route.test.js
+++ blogs-list/server/tests/integration/put_route.test.js
@@ -11,6 +11,7 @@ import app from "../../src/app.js";
 
 // Models
 import Blog from "../../src/models/blog.js";
+import User from "../../src/models/user.js";
 import initialBlogs from "../data/initialBlogs.js";
 import initialUsers from "../data/initialUsers.js";
 
@@ -210,5 +211,174 @@ describe("the Blogs PUT route", () => {
     // Assert the original likes counter has not been updated
     const originalBlog = await api.get(`/api/blogs/${postResponse.body.id}`);
     assert.strictEqual(postResponse.body.likes, originalBlog.body.likes);
+  });
+});
+
+describe("the Users PUT route", () => {
+  beforeEach(async () => {
+    // Remove all users before each test
+    await User.truncate({ restartIdentity: true, cascade: true });
+
+    // Create a new user to be updated
+    const userData = initialUsers[0];
+
+    postResponse = await api
+      .post("/api/users")
+      .send(userData)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("an user's name can be updated", async () => {
+    const newName = "New name";
+
+    // Get an user to be updated
+    const getResponse = await api
+      .get("/api/users/1")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    // Update an user
+    const putResponse = await api
+      .put(`/api/users/${getResponse.body.id}`)
+      .send({ name: newName })
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    // Confirm the name has been updated
+    assert.strictEqual(putResponse.body.name, newName);
+    assert.notStrictEqual(putResponse.body.name, getResponse.body.name);
+  });
+
+  test("only the name field should be updated when there are other parameters", async () => {
+    const updateData = {
+      username: "newusername@example.com",
+      name: "New name",
+      password: "newpassword"
+    };
+
+    // Get an user to be updated
+    const getResponse = await api
+      .get("/api/users/1")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    // Update an user
+    const putResponse = await api
+      .put(`/api/users/${getResponse.body.id}`)
+      .send(updateData)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    // Confirm only the name has been updated
+    assert.deepStrictEqual(
+      putResponse.body,
+      { ...getResponse.body, name: updateData.name, updatedAt: putResponse.body.updatedAt }
+    );
+    assert.notStrictEqual(putResponse.body, getResponse.body);
+  });
+
+  test("the updateAt field should be updated after a successful request", async () => {
+    const newName = "New name";
+
+    // Get an user to be updated
+    const getResponse = await api
+      .get("/api/users/1")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    // Update an user
+    const putResponse = await api
+      .put(`/api/users/${getResponse.body.id}`)
+      .send({ name: newName })
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    // Confirm the updateAt field has been updated
+    assert.deepStrictEqual(
+      putResponse.body,
+      { ...getResponse.body, name: newName, updatedAt: putResponse.body.updatedAt }
+    );
+    assert.notStrictEqual(putResponse.body.updatedAt, getResponse.body.updatedAt);
+  });
+
+  test("the createdAt field should not be updated", async () => {
+    const newName = "New name";
+
+    // Get an user to be updated
+    const getResponse = await api
+      .get("/api/users/1")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    // Update an user
+    const putResponse = await api
+      .put(`/api/users/${getResponse.body.id}`)
+      .send({ name: newName })
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    // Confirm the createAt field has not changed
+    assert.strictEqual(putResponse.body.createdAt, getResponse.body.createdAt);
+  });
+
+  test("an empty name should return a proper error message", async () => {
+    const newName = "";
+
+    // Get an user to be updated
+    const originalUserData = await api
+      .get("/api/users/1")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    // Update an user
+    const putResponse = await api
+      .put(`/api/users/${originalUserData.body.id}`)
+      .send({ name: newName })
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+
+    // Confirm the error message is within the response
+    assert.strictEqual(putResponse.body.error, "Invalid user's name");
+
+    const currentUserData = await api
+      .get("/api/users/1")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    // Assert the original name has not been changed
+    assert.strictEqual(originalUserData.body.name, currentUserData.body.name);
+
+    // Assert the updatedAt field has not changed
+    assert.strictEqual(originalUserData.body.updatedAt, currentUserData.body.updatedAt);
+  });
+
+  test("an undefined parameter should return a proper error message", async () => {
+    // Get an user to be updated
+    const originalUserData = await api
+      .get("/api/users/1")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    // Update an user
+    const putResponse = await api
+      .put(`/api/users/${originalUserData.body.id}`)
+      .send({})
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+
+    // Confirm the error message is within the response
+    assert.strictEqual(putResponse.body.error, "Invalid user's name");
+
+    const currentUserData = await api
+      .get("/api/users/1")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    // Assert the original name has not been changed
+    assert.strictEqual(originalUserData.body.name, currentUserData.body.name);
+
+    // Assert the updatedAt field has not changed
+    assert.strictEqual(originalUserData.body.updatedAt, currentUserData.body.updatedAt);
   });
 });
