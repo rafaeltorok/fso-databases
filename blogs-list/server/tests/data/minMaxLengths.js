6cfbbfc61936d3f94c86e80c9800b1986053af8b
Rafael Torok <rafaeltorok@gmail.com>
2026-06-13 09:19:59 -0300

    add: constants file to contain the min and max lengths for the models validations

---
+++ blogs-list/server/tests/data/minMaxLengths.js
@@ -0,0 +1,21 @@
+// Constants to define the min and max lengths for blogs and users fields
+
+// Blogs
+
+// Title
+export const minTitleLength = 3;
+export const maxTitleLength = 32;
+
+// Author
+export const minAuthorLength = 3;
+export const maxAuthorLength = 32;
+
+// Users
+
+// Username
+export const minUsernameLength = 5;
+export const maxUsernameLength = 32;
+
+// Name
+export const minNameLength = 3;
+export const maxNameLength = 32;
