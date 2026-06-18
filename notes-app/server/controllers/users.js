a371bfe4913f1e39e432f40823a75e5d1c80319a
rafaeltorok <rafaeltorok@gmail.com>
2026-06-18 18:08:39 -0300

    update: add admin-only route to enable and disable users - include extra code comments to better describe each route block

--- notes-app/server/controllers/users.js
+++ notes-app/server/controllers/users.js
@@ -5,9 +5,13 @@ import bcrypt from "bcrypt";
 // Models
 import { User, Note } from "../models/index.js";
 
+// Middleware
+import { tokenExtractor, isAdmin } from "../util/middleware.js";
+
 const usersRouter = express.Router();
 
 // Routes
+// Get all users
 usersRouter.get("/", async (req, res) => {
   const users = await User.findAll({
     attributes: {
@@ -23,6 +27,7 @@ usersRouter.get("/", async (req, res) => {
   res.json(users);
 });
 
+// Create a new user
 usersRouter.post("/", async (req, res) => {
   try {
     const { username, name, password } = req.body;
@@ -66,6 +71,7 @@ usersRouter.post("/", async (req, res) => {
   }
 });
 
+// Get an user based on its id
 usersRouter.get("/:id", async (req, res) => {
   const user = await User.findByPk(req.params.id, {
     attributes: {
@@ -77,6 +83,23 @@ usersRouter.get("/:id", async (req, res) => {
     res.json(user);
   } else {
     res.status(404).end();
+  }
+});
+
+// Admin-only route to enable or disable an user
+usersRouter.put("/:username", tokenExtractor, isAdmin, async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: req.params.username
+    }
+  });
+
+  if (user) {
+    user.disabled = req.body.disabled;
+    await user.save();
+    return res.status(200).json(user);
+  } else {
+    return res.status(404).end();
   }
 });
 
