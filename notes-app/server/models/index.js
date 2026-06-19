798f8609196830632c7b6af94eb03dcbb4dd7529
rafaeltorok <rafaeltorok@gmail.com>
2026-06-19 09:51:52 -0300

    update: add cascade option to remove all notes related to a removed user

--- notes-app/server/models/index.js
+++ notes-app/server/models/index.js
@@ -1,7 +1,9 @@
 import Note from "./note.js";
 import User from "./user.js";
 
-User.hasMany(Note);
+User.hasMany(Note, {
+  onDelete: "CASCADE",
+});
 Note.belongsTo(User);
 
 export { Note, User };
