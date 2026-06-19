dc3f2c981e048edb83168dcb1fbb8f4a082c3274
rafaeltorok <rafaeltorok@gmail.com>
2026-06-19 11:14:42 -0300

    update: add support for a test env variable definition on the db config

---
+++ notes-app/server/src/util/config.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const DATABASE_URL =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
+
+export const PORT = process.env.PORT || 3001;
+
+export const SECRET = process.env.SECRET;
