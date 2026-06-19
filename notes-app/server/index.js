75cb0a737ddb09b7bcf7f7c8f30c160d145c426b
rafaeltorok <rafaeltorok@gmail.com>
2026-06-19 11:16:22 -0300

    refactor: remove Express setup logic from the script

--- notes-app/server/index.js
+++ notes-app/server/index.js
@@ -1,25 +1,10 @@
-// Express
-import express from "express";
-const app = express();
+// Dependencies
+import app from "./src/app.js";
 
 // Utils
-import { PORT } from "./util/config.js";
-import { sequelize } from "./util/db.js";
-import runMigrations from "./util/migrations.js";
-
-// Controllers
-import notesRouter from "./controllers/notes.js";
-import usersRouter from "./controllers/users.js";
-import loginRouter from "./controllers/login.js";
-import healthRouter from "./controllers/health.js";
-
-app.use(express.json());
-
-// Routes
-app.use("/api/notes", notesRouter);
-app.use("/api/users", usersRouter);
-app.use("/api/login", loginRouter);
-app.use("/api/health", healthRouter);
+import { PORT } from "./src/util/config.js";
+import sequelize from "./src/util/db.js";
+import runMigrations from "./src/util/migrations.js";
 
 // Start the Express Server
 const start = async () => {
